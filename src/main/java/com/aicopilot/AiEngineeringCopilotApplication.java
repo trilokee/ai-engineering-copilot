b1a5fb5162e30720ec189169d9af907e0db2a8a6
trilokee <trilokeekumargupta@gmail.com>
2026-03-08 11:33:00 +0530

    reduced the ollama model size to 2.3 gb(phi3)

--- src/main/java/com/aicopilot/AiEngineeringCopilotApplication.java
+++ src/main/java/com/aicopilot/AiEngineeringCopilotApplication.java
@@ -1,4 +1,4 @@
-package com.aicopilot.aiengineeringcopilot;
+package com.aicopilot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
