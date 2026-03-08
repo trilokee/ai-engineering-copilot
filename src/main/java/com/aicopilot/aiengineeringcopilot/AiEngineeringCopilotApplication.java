53186d3622511e604df4601e497aea05b44a8400
trilokee <trilokeekumargupta@gmail.com>
2026-03-08 10:03:00 +0530

    initial commit

---
+++ src/main/java/com/aicopilot/aiengineeringcopilot/AiEngineeringCopilotApplication.java
@@ -0,0 +1,13 @@
+package com.aicopilot.aiengineeringcopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEngineeringCopilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiEngineeringCopilotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aicopilot/aiengineeringcopilot/AiEngineeringCopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aicopilot.aiengineeringcopilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEngineeringCopilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
